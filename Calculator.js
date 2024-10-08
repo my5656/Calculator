13db08dffe0bafc62a8fb751392574f710314d62
Megumi Yoda <>
2024-10-07 18:14:20 -0600

    Created operator functions on JS file and designed a calculator on the HTML and CSS files.

---
+++ Calculator.js
@@ -0,0 +1,27 @@
+const add = function (a, b) {
+    return a + b;
+};
+const subtract = function (a, b) {
+    return a - b;
+};
+const multiply = function (a, b) {
+    return a * b;
+};
+const divide = function (a, b) {
+    return a / b;
+};
+let firstNumber;
+let operator;
+let secondNumber;
+function operate(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+        case '-':
+            return subtract(firstNumber, secondNumber);
+        case '*':
+            return multiply(firstNumber, secondNumber);
+        case '/':
+            return divide(firstNumber, secondNumber);
+    }
+}
